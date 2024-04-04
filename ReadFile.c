f28985ae7a7fe5630bf853525d047ac9a625a056
Damian Śnieda <damiansnieda97@gmail.com>
2024-04-04 09:36:10 +0200

    Added a few small functions to split reponsibility

--- ReadFile.c
+++ ReadFile.c
@@ -2,17 +2,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+HANDLE OpenFileForAppendingAndReading(char* fileName)
 {
-    HANDLE hFile;
-    BOOL succesfulOperation;
-    char ReadBuffer[100];
-    DWORD dwBytesToWrite = 0;
-
-    SYSTEMTIME st;
-    
-    hFile = CreateFileA(
-        "C:\\Users\\Damian\\source\\repos\\SerialCommunication\\Driver\\test.txt",
+    HANDLE hFile = CreateFileA(
+        fileName,
         FILE_APPEND_DATA | FILE_READ_ACCESS,
         FILE_SHARE_READ,
         NULL,
@@ -26,24 +19,37 @@ int main()
         return 0; 
     }
 
+    return hFile;
+}
+
+char* BuildStringToWrite()
+{
+    SYSTEMTIME st;
+
+    GetLocalTime(&st);
+    char* ptr = (char*)calloc(100, sizeof(char));
+    sprintf(ptr, "Application running at %.2d.%.2d.%.4d  %.2d:%.2d:%.2d\n", st.wDay, st.wMonth, st.wYear, st.wHour, st.wMinute, st.wSecond);
+
+    return ptr;
+}
+
+int main()
+{
+    BOOL succesfulOperation;
+    char ReadBuffer[100];
+    DWORD dwBytesToWrite = 0;
+
+    HANDLE hFile = OpenFileForAppendingAndReading("C:\\Users\\Damian\\source\\repos\\SerialCommunication\\Driver\\test.txt");
     succesfulOperation = ReadFile(hFile, ReadBuffer, 99, NULL, NULL);
 
     for (int i=0 ; i<100; i++)
     {
-        GetLocalTime(&st);
-
-        char* ptr = (char*)calloc(100, sizeof(char));
-
-        sprintf(ptr, "Application running at %.2d.%.2d.%.4d  %.2d:%.2d:%.2d\n", st.wDay, st.wMonth, st.wYear, st.wHour, st.wMinute, st.wSecond);
-
+        char* ptr = BuildStringToWrite();
         dwBytesToWrite = (DWORD)strlen(ptr);
-
         succesfulOperation = WriteFile(hFile, ptr, dwBytesToWrite, NULL, NULL);
-
         free(ptr);
     }
 
     CloseHandle(hFile);
-
     return 0;
 }
